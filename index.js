a509eb280df0c5018ed76f228cc8b5ede5d13192
Pratyush Karn <pratyush.dainsta@gmail.com>
2025-06-14 01:24:35 +0530

    added new api

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.get("/",(req,res)=>
     return res.status(200).send('<h1>Hello World !!</h1>')
 })
 
+app.get("/ping",(req,res)=>res.status(200).send("<h1>Pong !!</h1>"));
 
 app.listen(4500, () => {
     console.log("App is Running At port 4500");
