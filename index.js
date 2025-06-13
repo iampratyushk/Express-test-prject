44f86f103f7cc4e15fe05aa16f2ab30953eac861
Pratyush Karn <pratyush.dainsta@gmail.com>
2025-06-14 00:58:14 +0530

    create hello world server

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+app.get("/",(req,res)=>
+{
+    return res.status(200).send('<h1>Hello World !!</h1>')
+})
+
+
+app.listen(4500, () => {
+    console.log("App is Running At port 4500");
+});
